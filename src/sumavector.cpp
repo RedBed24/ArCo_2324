9ef78f8d915e13024d0b23b83434b2b250c5a39c
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-10-14 20:33:14 +0200

    Reformat src/sumavector.cpp add return rm print

--- src/sumavector.cpp
+++ src/sumavector.cpp
@@ -1,26 +1,35 @@
-/* Programa secuencial que calcula la suma de los elementos de un vector v[i] 
- * y la almacena en la variable sum */ 
+/* Programa secuencial que calcula la suma de los elementos de un vector v[i] y la almacena en la variable sum */
 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <time.h> 
-#define N 50 // Probar distintos tamaños de vector 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N TAMAÑO // Probar distintos tamaños de vector
 
-void main() 
-{ 
-    int i, sum = 0; 
-    int v[N];    
+int main()
+{
+	int i, sum = 0;
+	int v[N];
 
-    // Damos valores aleatorios al vector (entre 0 y 99) 
-    srand (time(NULL)); // Semilla de números aleatorios 
-    for (i = 0; i < N; i++) v[i] = rand()%100; 
+	// Damos valores aleatorios al vector (entre 0 y 99)
+	srand(time(NULL)); // Semilla de números aleatorios
 
-    // Cálculo del sumatorio 
-    for (i = 0; i < N; i++) sum += v[i];           
+	#pragma omp simd
+	for (i = 0; i < N; i++) {
+		v[i] = rand() % 100;
+	}
 
-    // Como comprobación, se visualizan los valores del vector y la suma 
-    printf("\nVector de números: \n "); 
-    for (i = 0; i < N; i++) printf("%d \t",v[i]);
-    
-	printf("\n La suma es: %d \n\n", sum); 
+	// Cálculo del sumatorio
+	#pragma omp parallel for reduction(+:sum)
+	for (i = 0; i < N; i++) {
+		sum += v[i];
+	}
+
+	#pragma omp simd
+	for (i = 0; i < N; i++) {
+		printf("%d\t", v[i]);
+	}
+
+	printf("\n%d\n", sum); 
+	return 0;
 } 
+
