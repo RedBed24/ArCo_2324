b6375edf72bf2c48d703ba0d05024147c892bce7
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-10-14 20:31:27 +0200

    Add test for ej1 y 2

---
+++ src/comprobar_minmax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import sys
+
+with open(sys.argv[1], "r") as f:
+    valores = [int(x) for x in f.readline()[:-2].split()]
+
+    mini, maxi = f.readline()[:-1].split()
+
+    print(f"{int(mini) == min(valores)} {int(maxi) == max(valores)}")
+
---
+++ src/comprobar_suma.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import sys
+
+with open(sys.argv[1], "r") as f:
+    suma = sum([int(x) for x in f.readline()[:-2].split()])
+
+    print(f"{suma == int(f.readline()[:-1])}")
+
